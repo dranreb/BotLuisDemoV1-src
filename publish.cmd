007f11b90d50439ee71f54042b9564279293f872
Bernard Apolinario <dranreb@mail.com>
2018-07-12 10:34:29 -0700

    home automation change

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=botluisdemov1-Web-Deploy.pubxml -p:Password=hbfXjKjQvMQKzrDKBEC9Swgw1ACSWEEDX7pkct8hyrvc5og5uDeh92YsXMSq
+
